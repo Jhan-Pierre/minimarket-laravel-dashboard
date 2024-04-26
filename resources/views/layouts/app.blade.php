a65785030218acaee07026396dd2b50d75d4c321
luislopez-developer <luislopezhuari27@gmail.com>
2024-04-25 19:21:27 -0500

    implementar el dashboard a todas las vitas de manera dinamica

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -38,7 +38,14 @@
 
             <!-- Page Content -->
             <main>
-                {{ $slot }}
+
+                <div class="p-4 sm:ml-64">
+                    <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700">
+                        {{ $slot }}
+                    </div>
+                </div>
+
+                
             </main>
         </div>
         
