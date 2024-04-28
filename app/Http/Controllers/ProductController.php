b1c57525af1f048e2a763e629a911e0915cba708
luislopez-developer <luislopezhuari27@gmail.com>
2024-04-28 12:52:25 -0500

    se cambio los enlaces para habilitar los permisos

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -12,7 +12,7 @@ class ProductController extends Controller
     {
         /* $products = DB::select('call sp_list_products()');
         return view('products.index', compact('products')); */
-        return view('products.index');
+        return view('admin.products.index');
         //alternativas a compact
         //return view('students.index')->with('students', $students);
         //return view('students.index', ['students' => $students]);
@@ -20,12 +20,12 @@ class ProductController extends Controller
 
     public function create()
     {
-        return view('products.create');
+        /* return view('products.create'); */
     }
 
     public function store(Request $request)
     {
-        $request->validate([
+        /* $request->validate([
             'name' => 'required|string|min:3|max:100',
             'barcode' => 'required|string|min:6|max:50',
             'purchase_price' => 'required|numeric',
@@ -37,7 +37,7 @@ class ProductController extends Controller
 
         Product::create($request->all());
 
-        return redirect()->route('products.index');
+        return redirect()->route('products.index'); */
         
     }
 
@@ -48,13 +48,13 @@ class ProductController extends Controller
 
     public function edit(string $id)
     {
-        $product = Product::findorfail($id);
-        return view('products.edit', compact('product'));
+        /* $product = Product::findorfail($id);
+        return view('products.edit', compact('product')); */
     }
 
     public function update(Request $request, string $id)
     {
-        $request->validate([
+        /* $request->validate([
             'name' => 'required|string|min:3|max:100',
             'barcode' => 'required|string|min:6|max:50',
             'purchase_price' => 'required|numeric',
@@ -62,28 +62,20 @@ class ProductController extends Controller
             'stock' => 'required|integer',      
             'category_id' => 'required|integer',
             'state_id' => 'required|integer'
-        ]);
+        ]); */
 
-        $product = Product::FindOrFail($id);
-
-        $product->update($request->all());
-
-        return redirect()->route('products.index');
     }
 
     public function destroy(int $id)
     {
-        $product = Product::findorfail($id);
-        $product->delete();
-
-        return redirect()->route('products.index');
+;
     }
 
-    // Store procedures
+    /* // Store procedures
     public function sp_list_products(){
         $product = DB::select('call sp_list_products()');
 
         return view('products.index', compact('product'));
-    }
+    } */
 
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -15,6 +16,8 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('admin.dashboard');
+
+    Route::resource('users', UserController::class)->names('admin.users');
 
     Route::resource('products', ProductController::class)->names('admin.product');
 });
