3353b7d953cd59ca04516fdf91883af6f4eb8aae
luislopez-developer <luislopezhuari27@gmail.com>
2024-04-28 12:55:32 -0500

    limpieza de codigo inecesario

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -10,59 +10,28 @@ class ProductController extends Controller
 {
     public function index()
     {
-        /* $products = DB::select('call sp_list_products()');
-        return view('products.index', compact('products')); */
         return view('admin.products.index');
-        //alternativas a compact
-        //return view('students.index')->with('students', $students);
-        //return view('students.index', ['students' => $students]);
     }
 
     public function create()
     {
-        /* return view('products.create'); */
     }
 
     public function store(Request $request)
     {
-        /* $request->validate([
-            'name' => 'required|string|min:3|max:100',
-            'barcode' => 'required|string|min:6|max:50',
-            'purchase_price' => 'required|numeric',
-            'sale_price' => 'required|numeric',
-            'stock' => 'required|integer',      
-            'category_id' => 'required|integer',
-            'state_id' => 'required|integer'
-        ]);
 
-        Product::create($request->all());
-
-        return redirect()->route('products.index'); */
-        
     }
 
     public function show(string $id)
     {
-        //
     }
 
     public function edit(string $id)
     {
-        /* $product = Product::findorfail($id);
-        return view('products.edit', compact('product')); */
     }
 
     public function update(Request $request, string $id)
     {
-        /* $request->validate([
-            'name' => 'required|string|min:3|max:100',
-            'barcode' => 'required|string|min:6|max:50',
-            'purchase_price' => 'required|numeric',
-            'sale_price' => 'required|numeric',
-            'stock' => 'required|integer',      
-            'category_id' => 'required|integer',
-            'state_id' => 'required|integer'
-        ]); */
 
     }
 
@@ -70,12 +39,5 @@ class ProductController extends Controller
     {
 ;
     }
-
-    /* // Store procedures
-    public function sp_list_products(){
-        $product = DB::select('call sp_list_products()');
-
-        return view('products.index', compact('product'));
-    } */
 
 }
