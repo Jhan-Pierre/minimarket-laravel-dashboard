ca82a25b0820458fd07fb838b5b647331bac17be
luislopez-developer <luislopezhuari27@gmail.com>
2024-04-28 18:53:28 -0500

    mejora de estilos

--- resources/views/admin/products/index.blade.php
+++ resources/views/admin/products/index.blade.php
@@ -2,7 +2,7 @@
 
     <div class="max-w-screen-xl px-4 mx-auto lg:px-12 w-full">
         <x-slot name="header" >
-            <h2 class="text-4xl font-bold dark:text-white">
+            <h2 class="p-4 text-3xl font-extrabold leading-none tracking-tight text-gray-900 md:text-4xl dark:text-white">
                 {{ __('Products List') }}
             </h2>
         </x-slot>
