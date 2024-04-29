ad40b35b8e45dc32b54ce1ea1dafcf987d640230
luislopez-developer <luislopezhuari27@gmail.com>
2024-04-29 00:27:27 -0500

    se agrego funcion para eliminar usuario

--- app/Livewire/User/UserForm.php
+++ app/Livewire/User/UserForm.php
@@ -9,14 +9,31 @@ use Livewire\Attributes\Url;
 
 class UserForm extends Component
 {
-
     use WithPagination;
+
+    public $openDelete = false;
+    public $userDeleteId = "";
+    public $userDeleteName = "";
 
     #[Url(as: 's')]
     public $search = "";
 
     public function updatingSearch(){
         $this->resetPage();    
+    }
+
+    public function delete($userId, $userName)
+    {
+        $this->openDelete = true;
+        $this->userDeleteId = $userId;
+        $this->userDeleteName = $userName;
+    }
+
+    public function destroy()
+    {
+        User::destroy($this->userDeleteId);
+        $this->openDelete = false;
+        $this->reset(['userDeleteId', 'userDeleteName', 'openDelete']);
     }
 
     public function render()
