bb6d78534cf8241b74041ad9b4a36e11660b45fa
luislopez-developer <luislopezhuari27@gmail.com>
2024-05-02 17:26:21 -0500

    se mejoro la integridad del registrar venta

--- resources/views/livewire/sale/sale-create-form.blade.php
+++ resources/views/livewire/sale/sale-create-form.blade.php
@@ -8,12 +8,12 @@
                 @csrf
                 <div class="grid gap-4 mb-4 sm:grid-cols-2">
                     <div class="w-full">
-                        <label for="igv" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">IGV</label>
-                        <input value="18" type="number" id="igv" name="igv" step="0.01" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="0.0" required >
+                        <label for="igv" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" >IGV</label>
+                        <input value="18" disabled type="number" id="igv" name="igv" step="0.01" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="0.0" required >
                     </div>
                     <div class="w-full">
                         <label for="total" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Total</label>
-                        <input wire:model.live="total" type="number" id="total" name="total" step="0.01" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="0.0" required >
+                        <input wire:model.live="total" disabled type="number" id="total" name="total" step="0.01" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="0.0" required >
                     </div>
                     <div>
                         <x-label-flow>Metodos de pago</x-label-flow>
