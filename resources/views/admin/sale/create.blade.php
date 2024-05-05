9267eabb6c6a9320db43583f2fbdebdffc9011ba
luislopez-developer <luislopezhuari27@gmail.com>
2024-05-05 13:56:41 -0500

    se añadio el 'boton' de retroceder

--- resources/views/admin/sale/create.blade.php
+++ resources/views/admin/sale/create.blade.php
@@ -1,6 +1,5 @@
 <x-app-layout>
-    <div class="max-w-screen-xl px-4 mx-auto lg:px-12 w-full">
-        <h2 class="my-4 text-xl font-bold text-gray-900 dark:text-white">Registrar Venta</h2>
+    <div class="max-w-screen-xl px-4 mx-auto lg:px-12 w-full"> 
         @livewire('sale.sale-create');
     </div>
 </x-app-layout>
